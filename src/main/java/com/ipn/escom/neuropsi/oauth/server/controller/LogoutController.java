b0b39cf670fba98d25357088edacc7b772e801f6
Victor Mujica <victor.mujica.aztech@gmail.com>
2020-06-09 12:53:24 -0500

    logout

--- src/main/java/com/ipn/escom/neuropsi/oauth/server/controller/LogoutController.java
+++ src/main/java/com/ipn/escom/neuropsi/oauth/server/controller/LogoutController.java
@@ -1,6 +1,9 @@
 package com.ipn.escom.neuropsi.oauth.server.controller;
 
 import com.ipn.escom.neuropsi.oauth.server.controller.api.LogoutApi;
+import com.ipn.escom.neuropsi.oauth.server.entity.OauthAccessToken;
+import com.ipn.escom.neuropsi.oauth.server.repository.OauthAccessTokenRepository;
+import com.ipn.escom.neuropsi.oauth.server.repository.OauthRefreshTokenRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +13,17 @@ import java.security.Principal;
 @RestController
 @AllArgsConstructor
 public class LogoutController implements LogoutApi {
+
+    private final OauthAccessTokenRepository oauthAccessTokenRepository;
+    private final OauthRefreshTokenRepository oauthRefreshTokenRepository;
+
     @Override
     public ResponseEntity<Boolean> logout(Principal principal) {
-        return null;
+        OauthAccessToken oauthAccessToken = oauthAccessTokenRepository.findByUserName(principal.getName()).orElse(null);
+        if (oauthAccessToken == null) return ResponseEntity.ok(false);
+        String refreshToken = oauthAccessToken.getRefreshToken();
+        oauthRefreshTokenRepository.deleteById(refreshToken);
+        oauthAccessTokenRepository.deleteById(oauthAccessToken.getAuthenticationId());
+        return ResponseEntity.ok(true);
     }
 }
