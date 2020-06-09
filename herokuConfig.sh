c8416b20c6ea41645cf6b2e537e07e9d95e432ab
Victor Mujica <victor.mujica.aztech@gmail.com>
2020-06-09 14:50:31 -0500

    heroki config

--- herokuConfig.sh
+++ herokuConfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-heroku config:set USERNAME=oauth-server
-heroku config:set PASSWORD=e39aa8b32eaa3e570d93c4f0a30af38e
+heroku config:set USERNAME=neuropsi
+heroku config:set PASSWORD=0e4e034f3504195e386b1bdcf4fbfe78
 heroku config:set PROFILE=prod
 heroku config:set HOSTNAME=arcane-caverns-31516.herokuapp.com
-heroku config:set EUREKA_URL=https://eureka-server:b9cd4bdd5f554c576ba9e85b097f2be0@safe-shore-20278.herokuapp.com
+heroku config:set EUREKA_URL=https://neuropsi:0e4e034f3504195e386b1bdcf4fbfe78@safe-shore-20278.herokuapp.com
